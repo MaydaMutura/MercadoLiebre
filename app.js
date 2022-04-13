4f17b9fe9712c60e3a91a1f1589b75b158eb6d9b
MaydaMutura <mayda.mutura@gmail.com>
2022-04-14 01:47:52 +0200

    Subiendo el Proyecto

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require ("express");
+const path = require ("path");
+
+const app= express ();
+
+app.use(express.static(path.resolve(__dirname, "./public")))
+
+app.listen (3030, () => {
+    console.log ("Servidor corriendo en http://localhost:3030");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/home.html"));
+    
+   })
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve (__dirname, "./views/register.html"));
+ })
+ app.post("/register", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/home.html"));
+})
+    
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/login.html"));
+   })
+app.post("/login", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/home.html"));
+})
+
+app.get("/home", (req, res) => {
+    res.sendFile(path.resolve (__dirname, "./views/home.html"));
+   })
+app.post("/home", (req, res) => {
+    res.send ("Busqueda exitosa");
+})
+   
